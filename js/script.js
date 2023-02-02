f6a7e12dcd5fcf31b4d67ef74035108f5a8ad09a
Kerrion Siokwu <kerrionsiokwu@Kerrions-Mini.broadband>
2023-02-02 19:04:01 +0000

    changed bootstrap links

--- js/script.js
+++ js/script.js
@@ -158,26 +158,7 @@ $(document).ready(function () {
   
   
   
-      // cocktailTitleEl.text(response.strDrink.value);
-        //   $(".cocktail-title").text(data.strDrink);
-        //   $(".cocktail-instructions").text(data.strInstructions);
-        //   $(".cocktail-thumb").attr("src", data.strDrinkThumb);
-        //   $(".ingridient").empty();
-        //   $(".ingridient").append("<li>" + data.strIngredient1 + "</li>");
-        //   $(".ingridient").append("<li>" + data.strIngredient2 + "</li>");
-  
-  /* <section class="card row flex-row-reverse cocktail-card bg-light">
-        <img class="col-lg-4 card-img-end img-fluid p-0 cocktail-thumb" src="images/cocktail-image-example.jpg" />
-        <div class="col-lg-8 card-body">
-            <h2 class="card-title cocktail-title">Cocktail Title</h2>
-            <p class="card-text cocktail-instructions">Intructions for cocktail goes here</p>
-            <ul class="list-group-flush cocktail-ingridients">
-                <li class="ingridient">First ingridient</li>
-                <li class="ingridient">Second ingridient</li>
-            </ul>
-            <button class="btn custom-color" id="faveBtn">Add to favourites</button>
-            <button class="btn custom-color" id="rollAgainBtn">Roll again</button>
-        </div>
+
   
   
 
