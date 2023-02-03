5416529614d42f2e48f877fd19e2f52142fa8abb
Kerrion Siokwu <ladykerrion@sky.com>
2023-02-03 23:19:23 +0000

    code not working as expected committed so as not to loose track

--- js/faves.js
+++ js/faves.js
@@ -1,4 +1,4 @@
-
+/* 
 // retrieve the data from localStorage
 var cocktailData = JSON.parse(localStorage.getItem('cocktailData'));
 console.log(cocktailData);
@@ -41,4 +41,53 @@ $("#del-btn").on("click", function () {
     //TODO: Try to see if you could add more than one favourite cocktail. 
     //Todo: If you have more than one cocktail stored to local storage see if you could delete one and let the others remain. 
     //Todo: If this works, delete window.location.reload();
-});
+}); */
+
+
+// retrieve the data from localStorage
+var cocktailDataArray = JSON.parse(localStorage.getItem('cocktailData')) || [];
+console.log(cocktailDataArray);
+
+// show only the first 5 favorites
+var numFave = cocktailDataArray.length > 5 ? 5 : cocktailDataArray.length;
+
+for (var i = 0; i < numFave; i++) {
+  // create the card
+  var cardHTML = `
+    <div class="card m-2">
+      <img src="${cocktailDataArray[i].image}" class="card-img-top">
+      <div class="card-body">
+        <h2 class="card-title">${cocktailDataArray[i].title}</h2>
+        <ul class="card-text">
+          ${cocktailDataArray[i].ingredients}
+        </ul>
+        <p class="card-text">${cocktailDataArray[i].instructions}</p>
+      </div>
+    </div>
+  `;
+
+  // append the card to the #favorites-container
+  $("#favorites-container").append(cardHTML);
+}
+
+// create a delete buttton and append it to cardHTML
+var delBtn = $("<button>")
+    .addClass("btn custom-color btn-sm del-btn")
+    .appendTo(".card-body");
+
+delBtn.text("Delete");
+
+
+
+
+// Click handler for delete button 
+$(".del-btn").on("click", function (event) {
+    //localStorage.clear(); // clear local storage
+    event.target.localStorage.clear();
+    event.target.cardHTML.remove();
+    //window.location.reload(); // clears all favorites
+
+    //TODO: Try to see if you could add more than one favourite cocktail. 
+    //Todo: If you have more than one cocktail stored to local storage see if you could delete one and let the others remain. 
+    //Todo: If this works, delete window.location.reload();
+}); 
